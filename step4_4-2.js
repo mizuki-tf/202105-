8009fd5dd7e3a1082c119aa428b44f06abf56208
Mizuki Matsui <mizuki.tandf0910@gmail.com>
2021-05-07 12:11:25 +0900

    研修課題4章4-2

---
+++ step4_4-2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var nums = [4,5,6,9];
+
+let isEvenNumber = num => {
+  return num%2 === 0;
+}
+
+for(let i = 0; i<nums.length; i++){
+  if(isEvenNumber(nums[i])){
+    console.log(nums[i])
+  }
+}
